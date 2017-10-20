569397035f04d4eeb80dc9e6a5ceb2a069c7059f
Ash Blue <ash@blueashes.com>
2017-10-20 10:19:05 -0600

    Added static home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderDefaultComponent } from './modules/layouts/headers/header-default/header-default.component';
+import { FooterDefaultComponent } from './modules/layouts/footers/footer-default/footer-default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderDefaultComponent,
+    FooterDefaultComponent
   ],
   imports: [
     BrowserModule,
